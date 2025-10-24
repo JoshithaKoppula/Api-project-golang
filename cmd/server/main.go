d543c44c1d7da5bbe7f02a4a48deb45bd5c58d8b
JoshithaKoppula <kjoshithareddy28@gmail.com>
2025-10-24 12:58:09 +0530

    Fix : error handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,20 +6,19 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func main() {
-	// Initialize logger
 	logger.InitLogger()
 	defer logger.Logger.Sync()
-
 	app := fiber.New()
-
-	// Setup routes
 	routes.SetupRoutes(app)
 
 	logger.Logger.Info("Server running on :8080")
 	fmt.Println("Server running on :8080")
 
-	app.Listen(":8080")
+	if err := app.Listen(":8080"); err != nil {
+		logger.Logger.Fatal("Failed to start server", zap.Error(err))
+	}
 }
